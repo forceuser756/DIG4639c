9af4b37ce7a507aeaeb7f3529beda2fc238ae0cb
TFossum1 <TFossum1@gmail.com>
2019-09-25 18:27:44 -0400

    changes

--- lab2/src/components/Card/index.js
+++ lab2/src/components/Card/index.js
@@ -1,13 +1,12 @@
 import "./index.css";
 
 class Card {
-
   constructor(props){
     this.props = props;
   }
   render(){
     return `<div class="card">${this.props.content}</div>`;
   }
-  
+
 }
 export default Card;
